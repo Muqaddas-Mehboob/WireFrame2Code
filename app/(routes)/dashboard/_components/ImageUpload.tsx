5896dea71f121bdc84d32afa9b51e4a6df8040fe
Muqaddas Mehboob <muqaddasmehboob7@gmail.com>
2026-07-17 00:56:21 +0500

    Added User Input Fields inside the Wireframe UI and Made Changes to UI

--- app/(routes)/dashboard/_components/ImageUpload.tsx
+++ app/(routes)/dashboard/_components/ImageUpload.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { CloudUpload, X } from "lucide-react";
+import { CloudUpload, CodeXml, X } from "lucide-react";
 import Image from "next/image";
 import React, { ChangeEvent, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 function ImageUpload() {
+  const AIModelList = [
+    { name: "Google Gemini", value: "google-gemini", icon: "/gemini.jpg" },
+    { name: "Llama By Meta", value: "llama-meta", icon: "/meta.jpg" },
+    { name: "Deepseek", value: "deepseek", icon: "/deepseek.jpg" },
+  ];
   const [previewImage, setPreviewImage] = useState<string | null>(null);
 
   const onImageSelect = (e: ChangeEvent<HTMLInputElement>) => {
@@ -22,11 +36,9 @@ function ImageUpload() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         {!previewImage ? (
           <div className="p-7 border border-dashed rounded-md shadow-md">
-            <CloudUpload className="mx-auto mb-4" size={50} />
+            <CloudUpload className="mx-auto mb-4 text-primary" size={50} />
 
-            <h2 className="text-lg font-semibold text-center">
-              Upload Image
-            </h2>
+            <h2 className="text-lg font-semibold text-center">Upload Image</h2>
 
             <p className="text-center text-muted-foreground mt-3">
               Select a wireframe image to upload
@@ -34,7 +46,7 @@ function ImageUpload() {
 
             <div className="p-5 border border-dashed rounded-md mt-5 flex flex-col items-center justify-center gap-3">
               <label htmlFor="imageSelect" className="cursor-pointer">
-                <h2 className="p-2 bg-primary text-white rounded-md px-5">
+                <h2 className="p-2 bg-blue-100 text-primary rounded-md px-5 font-bold text-md">
                   Select Image
                 </h2>
               </label>
@@ -50,7 +62,8 @@ function ImageUpload() {
           </div>
         ) : (
           <div className="p-5 border border-dashed rounded-md shadow-md flex justify-center">
-            <X className="cursor-pointer"
+            <X
+              className="cursor-pointer"
               onClick={() => setPreviewImage(null)}
             />
             <Image
@@ -64,9 +77,45 @@ function ImageUpload() {
         )}
 
         <div className="p-7 border border-dashed rounded-lg shadow-md">
-          <h2 className="font-bold text-lg">Enter description about your Webpage</h2>
-          <Textarea className="mt-3 h-[200px]" placeholder="Write about your webpage" />
+          <h2 className="font-bold text-lg">AI Models</h2>
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select AI Model" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {AIModelList.map((model) => (
+                  <SelectItem key={model.value} value={model.value}>
+                    <div className="flex items-center gap-4">
+                      <Image
+                        src={model.icon}
+                        alt={model.name}
+                        width={25}
+                        height={25}
+                        className="rounded-sm"
+                      />
+                      <h2>{model.name}</h2>
+                    </div>
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <h2 className="font-bold text-lg mt-7">
+            Enter description about your Webpage
+          </h2>
+          <Textarea
+            className="mt-3 h-[200px]"
+            placeholder="Write about your webpage"
+          />
         </div>
+      </div>
+      <div className="flex items-center justify-center mt-10">
+        <Button className="font-semibold text-lg px-5 py-5 flex items-center gap-2">
+          <CodeXml />
+          Convert to Code
+        </Button>
       </div>
     </div>
   );
